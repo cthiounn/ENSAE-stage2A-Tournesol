fe3f434a10b592a82aa85983831a25d983c52411
cthiounn <thiounn.conrad@gmail.com>
2022-09-27 13:48:44 +0000

    pds

---
+++ public_database_simulation/script.sql
@@ -0,0 +1,34 @@
+--select * from core_user
+--where id IN
+--(
+--select user_id from (
+--select count(*) as nb_comparison,user_id from tournesol_comparison
+--group by user_id) as comp_count
+--where nb_comparison>=1000
+--)
+
+
+update core_user
+set is_superuser=True
+where id IN
+(
+select user_id from (
+select count(*) as nb_comparison,user_id from tournesol_comparison
+group by user_id) as comp_count
+where nb_comparison>=1000
+);
+
+--select * from core_user
+
+--tournesolpassword
+--pbkdf2_sha256$320000$2HrgDXi0wmCZkEXt0GKsRL$zQ6JDahm2NdGDanygDzZfIZxxzLutX2F7Q/2hVRkUlk=
+
+update core_user
+set password='pbkdf2_sha256$320000$2HrgDXi0wmCZkEXt0GKsRL$zQ6JDahm2NdGDanygDzZfIZxxzLutX2F7Q/2hVRkUlk=';
+
+create sequence id_seq;
+update core_user  
+  set username = 'user'||CAST(nextval('id_seq') as VARCHAR);
+drop sequence id_seq;
+
+select count(*) from core_user
