7342158b156625d712bdf9189f70da7af320b4fd
cthiounn <thiounn.conrad@gmail.com>
2022-09-28 06:21:19 +0000

    script sql++

--- public_database_simulation/script.sql
+++ public_database_simulation/script.sql
@@ -7,9 +7,12 @@
 --where nb_comparison>=1000
 --)
 
+insert into core_emaildomain (domain,status) values ('@example.fr','ACK');
 
+
+create sequence id_seq;
 update core_user
-set is_superuser=True
+set is_superuser=True,is_staff=True,email='admin'||CAST(nextval('id_seq') as VARCHAR)||'@example.fr'
 where id IN
 (
 select user_id from (
@@ -17,6 +20,7 @@ select count(*) as nb_comparison,user_id from tournesol_comparison
 group by user_id) as comp_count
 where nb_comparison>=1000
 );
+drop sequence id_seq;
 
 --select * from core_user
 
@@ -31,7 +35,7 @@ update core_user
   set username = 'user'||CAST(nextval('id_seq') as VARCHAR);
 drop sequence id_seq;
 
-select count(*) from core_user
+--select count(*) from core_user;
 
 create sequence id_seq;
 update tournesol_entity  
